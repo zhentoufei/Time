15546c0160bfc45126ef4f4cd38bb2a8167d0a6b
zhentoufei <zhentoufei@163.com>
2017-08-13 22:31:40 +0800

    数据理解

--- learn_data.py
+++ learn_data.py
@@ -9,3 +9,12 @@ __file__ = 'learn_data.py'
 
 import pandas as pd
 import numpy as np
+from config import get_data_absolute_path
+def read():
+    data_path = '{0}/train_1.csv'.format(get_data_absolute_path())
+    raw_data = pd.read_csv(data_path).fillna(0.0)
+    raw_data['col_sum'] =raw_data.apply(lambda x: x[1:].sum(), axis=1)
+    max_data = raw_data[raw_data['col_sum'] == raw_data['col_sum'].max()]
+
+if __name__ == '__main__':
+    read()
